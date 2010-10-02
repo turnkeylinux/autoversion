517daa91410dda8e648c33eb91ec8857b49b4b85
Liraz Siri <liraz@turnkeylinux.org>
2010-10-02 05:17:00 +0200

    optimized _git_rev_parse: skip git-rev-parse if given full commit id

--- pylib/autoversion.py
+++ pylib/autoversion.py
@@ -12,6 +12,10 @@ def _getstatusoutput(*command):
     return p.returncode, p.stdout.read().rstrip("\n")
     
 def _git_rev_parse(commit):
+    # skip expensive git-rev-parse if given a full commit id
+    if re.match('[0-9a-f]{40}$', commit):
+        return commit
+        
     error, output = _getstatusoutput("git-rev-parse", "--verify", commit)
     if error:
         return None
