fecfc0b533991e5e3803bca8cb357c4bee09a00c
Jeremy Davis <jeremy@turnkeylinux.org>
2020-09-13 20:23:24 +1000

    Adjust packaging.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+
+setup(
+    name="autoversion",
+    version="0.10.0",
+    author="Jeremy Davis",
+    author_email="jeremy@turnkeylinux.org",
+    url="https://github.com/turnkeylinux/autoversion",
+    packages=["autoversion_lib"],
+    scripts=["autoversion"]
+)
