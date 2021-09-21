ec3b6bd33afe3d0cef1b3ed7ef7898f527297905
Jeremy Davis <jeremy@turnkeylinux.org>
2021-09-21 11:43:15 +1000

    Rename executable back to autoversion.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 
 setup(
     name="autoversion",
-    version="0.10.0",
+    version="1.0",
     author="Jeremy Davis",
     author_email="jeremy@turnkeylinux.org",
     url="https://github.com/turnkeylinux/autoversion",
     packages=["autoversion_lib"],
-    scripts=["autoversion2"]
+    scripts=["autoversion"]
 )
